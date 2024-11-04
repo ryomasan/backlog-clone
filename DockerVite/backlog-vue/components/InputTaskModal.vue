725f239c502e643549a72e17644c15af039d45b4
R.Murakami <ryo1030ma2@gmail.com>
2024-11-05 03:19:23 +0900

    Add tag and task registration feature

--- DockerVite/backlog-vue/components/InputTaskModal.vue
+++ DockerVite/backlog-vue/components/InputTaskModal.vue
@@ -24,6 +24,14 @@
 </template>
 
 <script setup lang="ts">
+import axios from 'axios';
+
+interface Tag {
+    id: number,
+    name: string,
+    isSelected?: boolean
+}
+
 const props = defineProps<{
     isOpenModal: Boolean;
     taskStatus: string;
@@ -31,42 +39,59 @@ const props = defineProps<{
 
 const isOpenTagToggle = ref(false)
 
-const tags = reactive([
-    { name: "tag1", isSelected: true },
-    { name: "tag2", isSelected: false },
-    { name: "tag3", isSelected: false },
-])
+// let tags = reactive([
+//     { name: "tag1", isSelected: true },
+//     { name: "tag2", isSelected: false },
+//     { name: "tag3", isSelected: false },
+// ])
 
-const toggleTagList = () => {
+const tags = reactive<Tag[]>([]);
+
+const toggleTagList = async () => {
     isOpenTagToggle.value = !isOpenTagToggle.value
+    // await axios.get('http://localhost:8000/api/tags')
+    const response = await fetch('http://localhost:8000/api/tags');
+    try {
+        if (!response.ok) {
+            throw new Error("Failed to fetch tags");
+        }
+        const data = await response.json(); // Convert response to JSON
+        tags.splice(0, tags.length, ...data.map((tag: Tag) => ({ ...tag, isSelected: false }))); // Clear and update the tags
+    }
+    catch (error: any) {
+        console.error("Server responded with a status:", error);
+    };
 }
 
 const selectTags = (index: number) => {
     tags[index].isSelected = !tags[index].isSelected
 }
-
 const inputTask = ref<string>("");
 const tagName = ref<string>("");
 
-const createTag = (tagName: string) => {
+const createTag = async (tagName: string) => {
     if (tagName.trim()) {
         const newTag = {
             name: tagName,
-            isSelected: false
+        };
+        const response = await fetch('http://localhost:8000/api/tags', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json' // 送信するデータの形式
+            },
+            body: JSON.stringify(newTag) // 送信するデータをJSON形式に変換
+        });
+        try {
+            console.log(response);
+            if (!response.ok) {
+                throw new Error("Failed to fetch tags");
+            }
+            const data = await response.json();
+            tags.push(data);
+            // tags.splice(0, tags.length, ...data);
+        } catch (error: any) {
+            console.error("Server responded with a status:", error);
         }
-        tags.push(newTag);
-        console.log(tags)
-    };
-}
-
-
-
-const clickModal = (e: any) => {
-    // if (!e.target.classList.contains("modal-content")) {
-    //     emits("closeTaskModal");
-    // }
-    if (e.target.classList.contains("add-task-modal")) {
-        emits("closeTaskModal");
     }
 }
 
@@ -76,15 +101,23 @@ const closeTaskModal = (): void => {
     emits("closeTaskModal");
 }
 
+const clickModal = (e: any) => {
+    if (e.target.classList.contains("add-task-modal")) {
+        closeTaskModal();
+    }
+}
+
 const addTask = (newTask: Object): void => {
     if (inputTask) {
         const selectedTagsObj = tags.filter(tag => tag.isSelected === true);
         const selectedTags = selectedTagsObj.map(obj => Object.values(obj)[0])
+        console.log(inputTask);
         newTask = {
-            name: inputTask, tags: selectedTags, status: props.taskStatus
+            name: inputTask.value,
+            tags: selectedTags,
+            status: props.taskStatus
         };
         emits("addTask", newTask);
-        closeTaskModal();
     } else {
         alert('タスクが入力されていません');
     }
--- DockerVite/backlog-vue/pages/index.vue
+++ DockerVite/backlog-vue/pages/index.vue
@@ -7,21 +7,23 @@
         <div v-for="(status, index) in statusList" :key="index"
             class="bg-[#f0f0f0] p-[50px] text-3xl font-bold relative">
             {{ status.name }}
-            <TaskLists :status="status" :tasks="tasks" @openTaskModal="openTaskModal" />
+            <TaskLists :status="status" :tasks="tasks" @openTaskModal="openTaskModal"/>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-// import { ref } from 'vue'
-// const message = ref<string>("Hello world!")
+interface Tag {
+    id: number,
+    name: string,
+    isSelected?: boolean
+}
 
 interface Task {
     name: string;
-    tags: any; // tagsは文字列の配列
+    tags: Tag[];
     status: string;
 }
-
 
 const statusList = [
     { id: 1, name: "未対応" },
@@ -30,19 +32,19 @@ const statusList = [
     { id: 4, name: "完了" }
 ]
 
-const tasks = ref<Task[]>([
-    { name: "環境構築", tags: ["tag1", "tag2"], status: "未対応" },
-    { name: "トップ画面UI作成", tags: "tag1", status: "未対応" },
-    { name: "フィルター機能", tags: ["tag2", "tag3"], status: "レビュー中" },
-    { name: "タスク保存機能", tags: ["tag1", "tag3"], status: "完了" },
-]);
-// const tasks: Task[] = []
+// let tasks = ref<Task[]>([
+//     { name: "環境構築", tags: ["tag1", "tag2"], status: "未対応" },
+//     { name: "トップ画面UI作成", tags: "tag1", status: "未対応" },
+//     { name: "フィルター機能", tags: ["tag2", "tag3"], status: "レビュー中" },
+//     { name: "タスク保存機能", tags: ["tag1", "tag3"], status: "完了" },
+// ]);
+let tasks = reactive<Task[]>([]);
 
 const isOpenModal = ref<boolean>(false)
 
 let taskStatus = ref('未対応');
 
-const openTaskModal = (status:string): void => {
+const openTaskModal = (status: string): void => {
     isOpenModal.value = true
     taskStatus.value = status
 }
@@ -51,14 +53,52 @@ const closeTaskModal = (): void => {
     isOpenModal.value = false
 }
 
-// const getTasksByStatus = (statusName: string) => {
-//     console.log(tasks);
-//     return tasks.value.filter(task => task.status === statusName);
-// };
+const addTask = async (newTask: Task) => {
+    const getCircularReplacer = () => {
+        const seen = new WeakSet();
+        return (key: string, value: any) => {
+            if (typeof value === "object" && value !== null) {
+                if (seen.has(value)) {
+                    return; // 循環参照の場合、undefinedを返す
+                }
+                seen.add(value);
+            }
+            return value;
+        };
+    };
 
-const addTask = (newTask: any) => {
-    tasks.value.unshift(newTask);
-    console.log(tasks)
+    const response = await fetch('http://localhost:8000/api/tasks', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json' // 送信するデータの形式
+        },
+        body: JSON.stringify(newTask, getCircularReplacer())
+    });
+    try {
+        if (!response.ok) {
+            throw new Error("Failed to fetch tasks");
+        }
+        const data = await response.json();
+        tasks.unshift(data);
+        // tasks.splice(0, tasks.length, ...data);
+        closeTaskModal();
+    } catch (error: any) {
+        console.error("Server responded with a status:", error);
+    }
 }
 
+onMounted(async () => {
+    const response = await fetch('http://localhost:8000/api/tasks');
+    try {
+        if (!response.ok) {
+            throw new Error("Failed to fetch tags");
+        }
+        const data = await response.json(); // Convert response to JSON
+        console.log(data);
+        tasks.splice(0, tasks.length, ...data.map((task: Task) => ({ ...task }))); // Clear and update the tags
+    }
+    catch (error) {
+        console.error("Server responded with a status:", error);
+    }
+})
 </script>
